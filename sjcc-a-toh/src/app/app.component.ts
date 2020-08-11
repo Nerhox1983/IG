cca66ba8466c8696e25ca31ba83e10ae72511cea
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-11 09:33:30 -0500

    Actualización de heroes.component.html

--- sjcc-a-toh/src/app/app.component.ts
+++ sjcc-a-toh/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class AppComponent {
   title = 'Tour of heroes';
